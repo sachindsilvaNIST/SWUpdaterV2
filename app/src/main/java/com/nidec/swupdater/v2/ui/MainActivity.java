1c280f265be3fe4663e1019481768d0e1d535148
Sachin Dsilva <dsilva.sachinronson@nidec.com>
2025-02-25 09:19:58 +0900

    Fix FullScreen issue for [MainActivity]

--- app/src/main/java/com/nidec/swupdater/v2/ui/MainActivity.java
+++ app/src/main/java/com/nidec/swupdater/v2/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.nidec.swupdater.v2.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 
@@ -16,6 +17,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
 
