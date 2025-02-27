74cc8c638307297006a158d7b322f094b6f1cdc6
Sachin Dsilva <dsilva.sachinronson@nidec.com>
2025-02-27 18:44:06 +0900

    [Phase 1 - V2] : Add <OTAPackageAvailableActivity>, <ProgressScreenActivity>

--- app/src/main/java/com/nidec/swupdater/v2/ui/OTAPackageAvailableActivity.java
+++ app/src/main/java/com/nidec/swupdater/v2/ui/OTAPackageAvailableActivity.java
@@ -1,4 +1,86 @@
 package com.nidec.swupdater.v2.ui;
 
-public class OTAPackageAvailableActivity {
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.nidec.swupdater.v2.R;
+//import com.nidec.swupdater.v2.util.UpdateConfigs;
+//import com.nidec.swupdater.v2.UpdateConfig;
+
+
+
+public class OTAPackageAvailableActivity extends Activity {
+
+    private static final String TAG_OTA_PACKAGE_AVAILABLE_ACTIVITY = "OTAPackageAvailableActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ota_package_available);
+
+
+        Button mUpdateYesButton = findViewById(R.id.mOTAPackageAvailableYesButton);
+        Button mUpdateNoButton = findViewById(R.id.mOTAPackageAvailableNoButton);
+
+
+        // IF "YES" BUTTON WAS PRESSED : If the OTA Update Available --> Start the download process.
+        // E.g : UpdateManager.getInstance().startDownload()
+        // Goto Progress Screen Activity --> `ProgressScreenActivity.java`
+        mUpdateYesButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ProgressScreenActivity.class);
+            startActivity(intent);
+            finish();
+
+        });
+
+        // ===> Todo ---> DOUBT!!!!
+        // IF "NO" BUTTON WAS PRESSED : Cancel the update --> Back to "OTA Package Checker" Screen
+        // E.g : UpdateManager.getInstance().cancelDownload()
+        // Back to "OTA Package Checker" Screen --> `OTAPackageCheckerActivity.java`
+        // <=== Todo ---> DOUBT!!!!
+        mUpdateNoButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this,OTAPackageCheckerActivity.class);
+            startActivity(intent);
+            finish();
+
+        });
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
