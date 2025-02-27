5e0e8eee5d8d75342f29de8c073a7b740052b01f
Sachin Dsilva <dsilva.sachinronson@nidec.com>
2025-02-27 17:24:29 +0900

    [Phase 1 - V2] : Add <SystemUpToDateActivity>

---
+++ app/src/main/java/com/nidec/swupdater/v2/ui/SystemUpToDateActivity.java
@@ -0,0 +1,40 @@
+package com.nidec.swupdater.v2.ui;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.nidec.swupdater.v2.R;
+//import com.nidec.swupdater.v2.util.UpdateConfigs;
+//import com.nidec.swupdater.v2.UpdateConfig;
+
+public class SystemUpToDateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_up_to_date);
+
+        // Get the `ReCheck` Button UI Element ID
+        Button mReCheckButton = findViewById(R.id.mSystemUpToDateReCheckButton);
+
+        mReCheckButton.setOnClickListener(v -> {
+            // Re-check for OTA Package Updates.
+            // This will redirect again to `OTAPackageCheckerActivity.java` page.
+            Intent intent = new Intent(this, OTAPackageCheckerActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+    }
+}
