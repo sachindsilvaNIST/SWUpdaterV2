a7871a352e6d171b931288fa894818214c359580
Sachin Dsilva <dsilva.sachinronson@nidec.com>
2025-03-11 16:19:52 +0900

    <Phase 5: V2>[TYPO] Fix Typo <UpdateConfig.java>

--- src/com/nidec/swupdater/v2/UpdateConfig.java
+++ src/com/nidec/swupdater/v2/UpdateConfig.java
@@ -105,7 +105,7 @@ public class UpdateConfig implements Parcelable{
                         p.getString("filename"),
                         p.getLong("offset"),
                         p.getLong("size")
-                        ));
+                ));
             }
         }
 
@@ -130,7 +130,7 @@ public class UpdateConfig implements Parcelable{
 
 
     /**
-     * These strings represenst types in JSON Config files
+     * These strings represents types in JSON Config files
      */
 
     private static final String AB_INSTALL_TYPE_NON_STREAMING_JSON = "NON_STREAMING";
