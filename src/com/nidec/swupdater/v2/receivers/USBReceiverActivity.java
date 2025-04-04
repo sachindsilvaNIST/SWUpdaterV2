fd12796816ecc5bc392486d57ed817d61b2c91ed
Sachin Dsilva <dsilva.sachinronson@nidec.com>
2025-04-04 10:16:00 +0900

    <Phase 5: Alpha V2>[UPDATE] : ADD, IMPLEMENT USB Device Auto-launch operation <receivers/USBReceiverActivity.java, res/xml/usb_device_filter.xml, AndroidManifest.xml>

---
+++ src/com/nidec/swupdater/v2/receivers/USBReceiverActivity.java
@@ -0,0 +1,28 @@
+package com.nidec.swupdater.v2.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import android.hardware.usb.UsbDevice;
+
+import android.util.Log;
+
+import com.nidec.swupdater.v2.ui.RebootCheckActivity;
+
+
+public class USBReceiverActivity extends BroadcastReceiver {
+
+    private static final String TAG_USB_RECEIVER_ACTIVITY = "USBReceiverActivity";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if("android.hardware.usb.action.USB_DEVICE_ATTACHED".equals(intent.getAction())) {
+
+
+            Intent launchIntent = new Intent(context, RebootCheckActivity.class);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+        }
+    }
+}
